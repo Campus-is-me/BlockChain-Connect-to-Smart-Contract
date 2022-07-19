60377df4c63ff1feb6d0a77a1b74be61d72f9e2c
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-07-19 23:34:12 +0700

    Add files via upload

---
+++ Web3.py
@@ -0,0 +1,18 @@
+import http
+import json 
+from web3 import Web3
+
+from web3 import EthereumTesterProvider
+infura_url = 'https://mainnet.infura.io/v3/1e1c0742bfc64e6483a320b0547fcae9'
+web3 = Web3(Web3.HTTPProvider(infura_url))
+print(web3.isConnected())
+print(web3.eth.blockNumber)
+
+#creat wallet 
+i = 0
+while i < 100:
+    account = web3.eth.account.create()
+    address = account.address
+    private_key = account.privateKey
+    print(f"{address}:{private_key.hex()}")
+    i+=1 
